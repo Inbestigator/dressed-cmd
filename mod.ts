70af2932e0cedca5e13ec159531bb4f3b47e5735
Inbestigator <inbestigatoryt@gmail.com>
2025-01-20 13:10:52 -0800

    Runtime support

--- mod.ts
+++ mod.ts
@@ -4,6 +4,7 @@ import { build } from "@dressed/dressed/server";
 import { writeFile } from "node:fs/promises";
 import { green } from "@std/fmt/colors";
 import { dirname, join } from "node:path";
+import { cwd, exit } from "node:process";
 
 const program = new Command();
 
@@ -12,20 +13,14 @@ program.name("dressed").description("A serverless Discord bot framework.");
 program
   .command("build")
   .description("Builds the bot and writes to a bot.gen.ts")
-  .option(
-    "-i, --instance",
-    "Include an instance create in the generated file",
-  )
+  .option("-i, --instance", "Include an instance create in the generated file")
   .option("-r, --register", "Register slash commands")
   .action(async ({ instance, register }) => {
     const outputContent = await build(instance, register);
     const writing = ora("Writing to bot.gen.ts");
-    await writeFile(
-      "./bot.gen.ts",
-      new TextEncoder().encode(outputContent),
-    );
+    await writeFile("./bot.gen.ts", new TextEncoder().encode(outputContent));
     writing.succeed("Wrote to bot.gen.ts");
-    Deno.exit();
+    exit();
   });
 
 program
@@ -105,17 +100,15 @@ program
       const path =
         `https://api.github.com/repos/Inbestigator/dressed-examples/contents/${template}`;
 
-      await createFiles(path, join(Deno.cwd(), name));
+      await createFiles(path, join(cwd(), name));
     } catch {
       mkdirLoader.fail();
-      Deno.exit(1);
+      exit(1);
     }
     mkdirLoader.succeed();
 
-    console.log(
-      green("Project created successfully."),
-    );
-    Deno.exit();
+    console.log(green("Project created successfully."));
+    exit();
   });
 
 program.parse();
